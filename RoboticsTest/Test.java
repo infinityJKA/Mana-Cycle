bc7776497e6b7b94bf8dea81bb06d2b7a4353abe
Justout <47947043+infinityJKA@users.noreply.github.com>
2022-11-15 14:45:06 -0600

    yike!

---
+++ RoboticsTest/Test.java
@@ -0,0 +1,12 @@
+package RoboticsTest;
+
+import java.util.ArrayList;
+
+public class Test{
+    public static void Main (String[] args){
+        ArrayList<Integer> test = new ArrayList<Integer>();
+        test.add(1);
+        test.add(3);
+        test.add(5);
+    }
+}
