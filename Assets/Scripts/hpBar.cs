767303e287b61c77ab7e35aa28260c9e0b6ccf25
three-halves <96158142+three-halves@users.noreply.github.com>
2022-11-19 00:58:16 -0600

    working on hp bar

---
+++ Assets/Scripts/hpBar.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hpBar : MonoBehaviour
+{
+    [SerializeField] private GameObject hpDisp;
+    [SerializeField] private GameObject IncomingDmgDisp;
+    private Image hpBarImage;
+    private Image incomingDmgBarImage;
+    private RectTransform hpBarRectTransform;
+    private RectTransform incomingDmgBarRectTransform;
+    // using floats instead of ints to get percentage 
+    public float currentHp;
+    // just guessing max hp based on dmg formula, also might be different per character later on
+    public float maxHp = 1000f;
+    public float incomingDmg;
+    private float hpBarTopY;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        currentHp = maxHp;
+        incomingDmg = 0;
+
+        // get image components to edit attributes 
+        hpBarImage = hpDisp.GetComponent<Image>();
+        incomingDmgBarImage = IncomingDmgDisp.GetComponent<Image>();
+
+        // get rect transform to change positions later
+        hpBarRectTransform = hpDisp.GetComponent<RectTransform>();
+        incomingDmgBarRectTransform = IncomingDmgDisp.GetComponent<RectTransform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // percentage of hpbar and dmgbar that should be filled
+        hpBarImage.fillAmount = currentHp / maxHp;
+        incomingDmgBarImage.fillAmount = incomingDmg / maxHp;
+        
+        // placement of dmg indicator, top of current hp bar
+        hpBarTopY = hpBarImage.fillAmount * hpBarRectTransform.localScale.y - hpBarRectTransform.localScale.y + 0.5f; // idk why you need the 0.5 in there i just adjusted it until it worked (lol)
+        // change anchors of bar to update position
+        incomingDmgBarRectTransform.anchorMin = new Vector2(incomingDmgBarRectTransform.anchorMin.x, hpBarTopY);
+        incomingDmgBarRectTransform.anchorMax = new Vector2(incomingDmgBarRectTransform.anchorMax.x, hpBarTopY);
+        incomingDmgBarRectTransform.anchoredPosition = Vector2.zero;
+        // Debug.Log(hpBarTopY);
+    }
+}
