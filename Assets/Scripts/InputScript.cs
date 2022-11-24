2789e69bb2c1fda9e76653cfa3df2a4f901f6cc1
three-halves <96158142+three-halves@users.noreply.github.com>
2022-11-23 21:32:49 -0600

    2 player functionality (no ai yet)

---
+++ Assets/Scripts/InputScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputScript : MonoBehaviour
+{
+    public KeyCode Left;
+    public KeyCode Right;
+    public KeyCode Down;
+    public KeyCode RotateLeft;
+    public KeyCode RotateRight;
+    public KeyCode Cast;
+
+}
