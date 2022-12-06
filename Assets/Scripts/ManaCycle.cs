264d1f9db9b557c2083930fdf8018149e13649a2
caesarj <jackachulian@gmail.com>
2022-12-06 16:24:40 -0600

    Fixed manaCycle rng

--- Assets/Scripts/ManaCycle.cs
+++ Assets/Scripts/ManaCycle.cs
@@ -18,7 +18,7 @@ public class ManaCycle : MonoBehaviour
     // List of all cycleColor objects that represent the colors
     private List<Image> cycleObjects;
     // Length of the cycle
-    private int cycleLength = 7;
+    public static int cycleLength = 7;
     void Start()
     {
         // Generate cycle that boards will use
@@ -55,26 +55,64 @@ public class ManaCycle : MonoBehaviour
         // Initialize a new list (will override old ones)
         cycle = new List<ManaColor>();
         // Start color at -1, or no color, so color first can be any color
-        int color = -1;
-        int[] colors = new int[cycleLength];
-        for (int i=0; i<cycleLength; i++) {
-            int newColor = color;
-            // Set color to random until it is different than the last color
-            while (newColor == color)
+        // int color = -1;
+        // int[] colors = new int[cycleLength];
+        // for (int i=0; i<cycleLength; i++) {
+        //     int newColor = color;
+        //     // Set color to random until it is different than the last color
+        //     while (newColor == color)
+        //     {
+        //         newColor = Random.Range(0,5);
+        //         if (checkCountInArray(colors,newColor) >= 2){
+        //             newColor = color;
+        //         }
+        //     }
+
+        //     Debug.Log(color);
+        //     color = newColor;
+        //     cycle.Add((ManaColor) color);
+
+        //     // Create cycle color object and add to this object as a child
+        //     Image cycleObject = Instantiate(cycleColorPrefab, Vector3.zero, Quaternion.identity);
+        //     cycleObject.color = manaColors[color];
+        //     cycleObjects.Add(cycleObject);
+        //     cycleObject.transform.SetParent(transform, false);
+        // }
+
+        // Add one of each color to the list
+        for (int i=0; i<5; i++)
+        {
+            cycle.Add((ManaColor)i);
+        }
+
+        // Add two more random colors
+        for (int i=0; i<2; i++)
+        {
+            cycle.Add((ManaColor)Random.Range(0,5));
+        }
+
+        // Shuffle the list
+        Utils.Shuffle(cycle);
+
+        // For each color, check that the color below is not the same color
+        for (int i=0; i<cycleLength-1; i++)
+        {
+            // If it is, swap the color to a random color that is not either of the colors next to it
+            // If at the top, tile above is the tile at the bottom, which is the one before it
+            ManaColor colorAbove = (i == 0) ? cycle[cycle.Count-1] : cycle[i-1];
+            ManaColor colorBelow = cycle[i+1];
+
+            while (cycle[i] == colorAbove || cycle[i] == colorBelow)
             {
-                newColor = Random.Range(0,5);
-                if (checkCountInArray(colors,newColor) >= 2){
-                    newColor = color;
-                }
+                cycle[i] = (ManaColor)Random.Range(0,5);
             }
+        }
 
-            Debug.Log(color);
-            color = newColor;
-            cycle.Add((ManaColor) color);
-
-            // Create cycle color object and add to this object as a child
+        // Create cycle color objects for each cycle color
+        for (int i=0; i<cycleLength; i++)
+        {
             Image cycleObject = Instantiate(cycleColorPrefab, Vector3.zero, Quaternion.identity);
-            cycleObject.color = manaColors[color];
+            cycleObject.color = manaColors[(int)cycle[i]];
             cycleObjects.Add(cycleObject);
             cycleObject.transform.SetParent(transform, false);
         }
