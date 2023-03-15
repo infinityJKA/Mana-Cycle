aca15f64cd19121fa257fc2454a12e01210e62fd
three-halves <threehalves1@gmail.com>
2023-03-15 08:29:23 -0500

    forgor to add files on last commit :skull:

---
+++ Assets/Scripts/CharSelectScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CharSelectScript : MonoBehaviour
+{
+    [SerializeField] private InputScript inputScript;
+    [SerializeField] private List<Battler> battlerList;
+    [SerializeField] private GameObject portraitDisp;
+    [SerializeField] private GameObject nameDisp;
+    private TMPro.TextMeshProUGUI nameText;
+
+    private int charSelection = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nameText = nameDisp.GetComponent<TMPro.TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(inputScript.Left))
+        {
+            charSelection--;
+        }
+
+        if (Input.GetKeyDown(inputScript.Right))
+        {
+            charSelection++;
+        }
+
+        // keep selection in bounds
+        charSelection = Math.Abs(charSelection) % battlerList.Count;
+
+        // update objects
+        Battler currentChar = battlerList[charSelection];
+        nameText.text = currentChar.displayName;
+    }
+}
