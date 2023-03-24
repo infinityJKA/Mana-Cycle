646e70ce2092f29f5211baa0e65e04781dbd4360
Justout <jkajustin@gmail.com>
2023-03-24 18:13:25 -0500

    -Added the question mark and gear models, adding cinemachine support for the fourth option (settings w/the gear model). -Added the description text that changes depending on which option is highlighted. -Adjusted the cinemachine transition speed/smoothness. -Adjusted some of the cinemachine camera positions. TODO for me: -Work on sfx -Work on character portraits

--- Assets/Scripts/MenuButtonCam.cs
+++ Assets/Scripts/MenuButtonCam.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 using Cinemachine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using TMPro;
 
 
  public class MenuButtonCam : MonoBehaviour, ISelectHandler
  {
     public CinemachineVirtualCamera thisCam;
     public CinemachineBrain brain;
+    public TMP_Text textBox;
+    public string text;
 
     public void OnSelect(BaseEventData eventData){
         // if(eventData.selectedObject == this.gameObject){
             brain.ActiveVirtualCamera.Priority = 1;
             thisCam.Priority = 30;
+            textBox.text = text;
         // } 
     }
 }
