f8cea11b944454800dc089c96a9a7e818e721173
caesarj <jackachulian@gmail.com>
2023-03-28 13:50:09 -0500

    still hate github (but working i think?)

---
+++ Assets/Scripts/GradientObject.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace GradientSkybox {
+    public class GradientObject : ScriptableObject
+    {
+        public Gradient gradient = new Gradient();
+    }
+
+}
