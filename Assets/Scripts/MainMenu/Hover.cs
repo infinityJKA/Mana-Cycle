1421de42ffd630d2fb0f27d5fb0b4862d6e061ed
three-halves <threehalves1@gmail.com>
2023-03-28 08:53:54 -0500

    make hover script use variables for speed and range

--- Assets/Scripts/MainMenu/Hover.cs
+++ Assets/Scripts/MainMenu/Hover.cs
@@ -8,6 +8,8 @@ public class Hover : MonoBehaviour
     private float y;
     private float t;
     private float originY;
+    [SerializeField] private float scale;
+    [SerializeField] private float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +21,7 @@ public class Hover : MonoBehaviour
     void Update()
     {
         t += Time.deltaTime;
-        y = (float) Math.Sin(t*2) / 2;
+        y = (float) Math.Sin(t*speed) * scale;
         transform.position = new Vector3(transform.position.x, originY + y, transform.position.z);
     }
 }
