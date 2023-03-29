0ee2701af24b0e590b4bc0e1eb9acd0eb230b668
caesarj <jackachulian@gmail.com>
2023-03-29 10:01:16 -0500

    timer works, fixed pointer, timer color

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -14,16 +14,31 @@ public class Timer : MonoBehaviour
     /** Text to update timer text on */
     [SerializeField] private TMPro.TextMeshProUGUI textbox;
 
+    /** Normal color shown */
+    [SerializeField] private TMPro.TMP_ColorGradient greenGradient;
+
+    /** Color shown when time is almost up */
+    [SerializeField] private TMPro.TMP_ColorGradient redGradient;
+
     /** if the timer is currently running */
     private bool running = false;
 
     // The time the timer will end at.
     private float endTime;
 
+    void Start() {
+        textbox.enableVertexGradient = true;
+        textbox.colorGradientPreset = greenGradient;
+    }
+
     public void StartTimer() 
     {
         running = true;
         endTime = Time.time + duration;
+    }
+
+    public void StopTimer() {
+        running = false;
     }
 
     // Update is called once per frame
@@ -32,6 +47,10 @@ public class Timer : MonoBehaviour
         if (!running) return;
 
         float timeLeft = endTime - Time.time;
+
+        if (timeLeft <= 20) {
+            textbox.colorGradientPreset = redGradient;
+        }
 
         if (timeLeft <= 0) {
             textbox.text = "0:00";
