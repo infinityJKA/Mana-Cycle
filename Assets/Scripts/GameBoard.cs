7c7fab499e8d673ae1e3aa804da3a9687910311a
caesarj <jackachulian@gmail.com>
2023-03-31 20:43:57 -0500

    level changes, added dim masks

--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -675,7 +675,7 @@ public class GameBoard : MonoBehaviour
     }
 
     IEnumerator CheckMidConvoAfterDelay() {
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(0.4f);
         CheckMidLevelConversations();
     }
 
