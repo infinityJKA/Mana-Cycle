40113c65814301a20469e31df377cc8b8d126532
three-halves <threehalves1@gmail.com>
2023-04-01 00:41:54 -0500

    fix bug with pitched sfx effect other sfx

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -18,8 +18,12 @@ public class SoundManager : MonoBehaviour
     }
 
     public void PlaySound(AudioClip clip, float pitch = 1f){
-        effectSource.pitch = pitch;
-        effectSource.PlayOneShot(clip);
+        // create a new gameobject with an audiosource, to avoid interfering with other sound effects
+        GameObject tempEffectSource = new GameObject("tempEffectSource");
+        tempEffectSource.AddComponent<AudioSource>();
+        tempEffectSource.GetComponent<AudioSource>().pitch = pitch;
+        tempEffectSource.GetComponent<AudioSource>().PlayOneShot(clip);
+        Destroy(tempEffectSource, clip.length);
     }
 
     public void ChangeMusicVolume(float value){
