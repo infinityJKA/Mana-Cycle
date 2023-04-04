7feb20920cd7e715226f05675df75b34332975ac
caesarj <jackachulian@gmail.com>
2023-04-04 11:27:03 -0500

    fixed infinite scroll

--- Assets/Scripts/ScrollingTerrain.cs
+++ Assets/Scripts/ScrollingTerrain.cs
@@ -7,15 +7,15 @@ public class ScrollingTerrain : MonoBehaviour {
     // Z movement speed
     public float speed;
     // Amount of offset with respect to time
-    private float offset;
-
-    void Start() {
-        offset = transform.position.z;
-    }
+    private static float offset = 1000;
 
     void Update()
     {
-        float z = speed*Time.time + offset;
-        transform.position = new Vector3(0, 0, z);
+        transform.Translate(speed * Vector3.back * Time.unscaledDeltaTime);
+        if (transform.position.z > offset) {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - offset*2);
+        } else if (transform.position.z < -offset) {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + offset*2);
+        }
     }
 }
