0ecc19d010b5c157eeb754467fd903f0303f3f02
caesarj <jackachulian@gmail.com>
2023-04-06 17:04:20 -0500

    levellister matches screen size, better scroll

--- Assets/Scripts/LevelLister.cs
+++ Assets/Scripts/LevelLister.cs
@@ -12,9 +12,10 @@ public class LevelLister : MonoBehaviour
     [SerializeField] private Level[] levelsList;
 
     // List level object caches
-    [SerializeField] private GameObject listObject;
-    private TMPro.TextMeshProUGUI listText;
-    private RectTransform listTransform;
+    /** list text component where the lines are written out */
+    [SerializeField] private TMPro.TextMeshProUGUI listText;
+
+    // self explanatory
     [SerializeField] private TMPro.TextMeshProUGUI descriptionText;
     [SerializeField] private TMPro.TextMeshProUGUI timeText;
 
@@ -23,11 +24,14 @@ public class LevelLister : MonoBehaviour
 
     /** Inputs that control the level list */
     [SerializeField] private InputScript[] inputScripts;
-    /** y offset when scrolling (?) */
-    [SerializeField] private float yOffset;
+
+    /** Initial offset of the list, saved on start from anchored position */
+    [SerializeField] private Vector2 listOffset;
+    /** amount to scroll by for each line. set to font size on start */
     [SerializeField] private float scrollAmount;
+
     /** Index of current level selected */
-    private int selection;
+    private int selectedLevelIndex;
     /** Descent between lines in the level list */
     // private float decLine;
 
@@ -39,26 +43,28 @@ public class LevelLister : MonoBehaviour
     /** If the level list is currently focused, instead of dialogue */
     private bool focused; 
 
+    // menu sfx
     [SerializeField] private AudioClip moveSFX;
     [SerializeField] private AudioClip selectSFX;
     [SerializeField] private AudioClip errorSFX;
 
+    // currently unused references
     [SerializeField] GameObject upArrow;
-
     [SerializeField] GameObject downArrow;
 
     // Start is called before the first frame update
     void Start()
     {
         focused = true;
-        listText = listObject.GetComponent<TMPro.TextMeshProUGUI>();
         // decLine = (listText.font.faceInfo.descentLine);
-        listTransform = listObject.GetComponent<RectTransform>();
 
         // last level selected when in this window.
         if (Storage.lastLevelSelectedIndex == -1) Storage.lastLevelSelectedIndex = GetNextLevel();
-        else selection = Storage.lastLevelSelectedIndex;
-        Storage.lastLevelSelectedIndex = selection;
+        else selectedLevelIndex = Storage.lastLevelSelectedIndex;
+        Storage.lastLevelSelectedIndex = selectedLevelIndex;
+
+        listOffset = listText.rectTransform.anchoredPosition;
+        scrollAmount = listText.fontSize;
 
         RefreshList();
     }
@@ -72,16 +78,16 @@ public class LevelLister : MonoBehaviour
 
             if (Input.GetKeyDown(inputScript.Up))
             {
-                selection--;
-                Storage.lastLevelSelectedIndex = selection;
+                selectedLevelIndex--;
+                Storage.lastLevelSelectedIndex = selectedLevelIndex;
                 RefreshList();
                 SoundManager.Instance.PlaySound(moveSFX, pitch : 1.1f);
             }
 
             if (Input.GetKeyDown(inputScript.Down))
             {
-                selection++;
-                Storage.lastLevelSelectedIndex = selection;
+                selectedLevelIndex++;
+                Storage.lastLevelSelectedIndex = selectedLevelIndex;
                 RefreshList();
                 SoundManager.Instance.PlaySound(moveSFX);
             }
@@ -95,15 +101,15 @@ public class LevelLister : MonoBehaviour
             // cast - open selected level
             if (Input.GetKeyDown(inputScript.Cast))
             {
-                if (!levelsList[selection].RequirementsMet()) 
+                if (!levelsList[selectedLevelIndex].RequirementsMet()) 
                 {
                     SoundManager.Instance.PlaySound(errorSFX);
                     return;
                 }
                 
-                Storage.level = levelsList[selection]; 
+                Storage.level = levelsList[selectedLevelIndex]; 
                 Storage.gamemode = Storage.GameMode.Solo;
-                convoHandler.StartLevel(levelsList[selection]);
+                convoHandler.StartLevel(levelsList[selectedLevelIndex]);
                 focused = false;
                 Storage.levelSelectedThisInput = true;
                 SoundManager.Instance.PlaySound(selectSFX);
@@ -111,16 +117,19 @@ public class LevelLister : MonoBehaviour
         }
 
         // smoothly update displayed y position of level list
-        listTransform.position = Vector2.SmoothDamp(listTransform.position, targetPosition, ref vel, 0.1f);
+        listText.rectTransform.anchoredPosition = 
+        Vector2.SmoothDamp(listText.rectTransform.anchoredPosition, targetPosition, ref vel, 0.1f);
     }
 
+
+    private static int flavorLineCount = 30;
     void RefreshList()
     {
-        selection = Math.Clamp(selection, 0, levelsList.Length-1);
+        selectedLevelIndex = Math.Clamp(selectedLevelIndex, 0, levelsList.Length-1);
         
         string newText = "";
         // add and subtract for extra lines at the start and end of list
-        for (int i = -30; i < levelsList.Length + 30; i++)
+        for (int i = -flavorLineCount; i < levelsList.Length + flavorLineCount; i++)
         {
             // flavor lines
             if (i < 0 || i >= levelsList.Length){
@@ -131,7 +140,7 @@ public class LevelLister : MonoBehaviour
 
                 bool cleared = PlayerPrefs.GetInt(level.GetInstanceID()+"_Cleared", 0) == 1;
 
-                if (i == selection && level.RequirementsMet()) newText += " <color=#FFFFFF>";
+                if (i == selectedLevelIndex && level.RequirementsMet()) newText += " <color=#FFFFFF>";
 
                 else if (!level.RequirementsMet()) newText += "<color=#015706>";
 
@@ -139,7 +148,7 @@ public class LevelLister : MonoBehaviour
 
                 newText += level.levelName;
 
-                if (i == selection) newText += " <";
+                if (i == selectedLevelIndex) newText += " <";
                 newText += " </color>";
 
                 // clear check
@@ -159,7 +168,7 @@ public class LevelLister : MonoBehaviour
         listText.text = newText;
 
         // display the description and time of the selected level
-        Level selectedLevel = levelsList[selection];
+        Level selectedLevel = levelsList[selectedLevelIndex];
         descriptionText.text = selectedLevel.description;
 
         bool selectedCleared = PlayerPrefs.GetInt(selectedLevel.GetInstanceID()+"_Cleared", 0) == 1;
@@ -177,7 +186,8 @@ public class LevelLister : MonoBehaviour
         
 
         // update the targeted scroll position
-        targetPosition = new Vector2(listTransform.position.x, selection*(scrollAmount)*Screen.height + yOffset*Screen.height);
+        targetPosition = listOffset + Vector2.up * (flavorLineCount+selectedLevelIndex) * scrollAmount;
+        // targetPosition = new Vector2(listTransform.position.x, selectedLevelIndex*(scrollAmount)*Screen.height + yOffset*Screen.height);
     }
 
     public void SetFocus(bool f){
