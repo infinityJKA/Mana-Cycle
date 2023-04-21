c6970cd1d5052bb00faf6318bc3bc28af4e86d72
caesarj <jackachulian@gmail.com>
2023-04-21 12:53:11 -0500

    moved mainmenu scripts into namespace general code cleanup going to move scripts into their own namespaced folders next

--- Assets/Scripts/MainMenu/Hover.cs
+++ Assets/Scripts/MainMenu/Hover.cs
@@ -1,27 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class Hover : MonoBehaviour
-{
-    private float y;
-    private float t;
-    private float originY;
-    [SerializeField] private float scale;
-    [SerializeField] private float speed;
-    // Start is called before the first frame update
-    void Start()
+namespace MainMenu {
+    /** Used to make the question mark in the homepage hover. */
+    public class Hover : MonoBehaviour
     {
-        t = 0;
-        originY = transform.position.y;
-    }
+        private float y;
+        private float t;
+        private float originY;
+        [SerializeField] private float scale;
+        [SerializeField] private float speed;
+        // Start is called before the first frame update
+        void Start()
+        {
+            t = 0;
+            originY = transform.position.y;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        t += Time.deltaTime;
-        y = (float) Math.Sin(t*speed) * scale;
-        transform.position = new Vector3(transform.position.x, originY + y, transform.position.z);
+        // Update is called once per frame
+        void Update()
+        {
+            t += Time.deltaTime;
+            y = (float) Math.Sin(t*speed) * scale;
+            transform.position = new Vector3(transform.position.x, originY + y, transform.position.z);
+        }
     }
 }
