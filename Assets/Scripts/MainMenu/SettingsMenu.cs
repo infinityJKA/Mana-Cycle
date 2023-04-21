c0a6328ec08d1ed7ac8eae4db7417e6ed472b2fa
caesarj <jackachulian@gmail.com>
2023-04-21 12:53:26 -0500

    namespacing

--- Assets/Scripts/MainMenu/SettingsMenu.cs
+++ Assets/Scripts/MainMenu/SettingsMenu.cs
@@ -1,16 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class SettingsMenu : MonoBehaviour
-{
-    [SerializeField] private InputScript inputScript;
-    [SerializeField] private UnityEngine.UI.Button closeButton;
-    
-    void Update()
+namespace MainMenu {
+    /// <summary>
+    /// Controls the settings menu, currently only the close button. (Slider volumes are handled solely by SoundManager)
+    /// </summary>
+    public class SettingsMenu : MonoBehaviour
     {
-        if (Input.GetKeyDown(inputScript.Pause)) {
-            closeButton.onClick.Invoke();
+        [SerializeField] private InputScript inputScript;
+        [SerializeField] private UnityEngine.UI.Button closeButton;
+        
+        void Update()
+        {
+            if (Input.GetKeyDown(inputScript.Pause)) {
+                closeButton.onClick.Invoke();
+            }
         }
     }
 }
