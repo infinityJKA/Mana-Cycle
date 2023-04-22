dd4b344f0b6fbbd4f45cec2a1ebaf2b98a47c5d8
three-halves <threehalves1@gmail.com>
2023-04-22 10:30:17 -0500

    setup reset to default backend

--- Assets/Scripts/InputScript.cs
+++ Assets/Scripts/InputScript.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+#if (UNITY_EDITOR)
+using UnityEditor;
+#endif
+
 [CreateAssetMenu(fileName = "Battler", menuName = "ManaCycle/Input Script")]
 public class InputScript : ScriptableObject
 {
+    public InputScript defaults;
+
     public KeyCode Left;
     public KeyCode Right;
     public KeyCode Down;
@@ -13,5 +19,37 @@ public class InputScript : ScriptableObject
     public KeyCode RotateRight;
     public KeyCode Cast;
     public KeyCode Pause;
+
+    public void resetToDefault()
+    {
+        if (defaults == null) return;
+
+        Left = defaults.Left;
+        Right = defaults.Right;
+        Down = defaults.Down;
+        Up = defaults.Up;
+        RotateLeft = defaults.RotateLeft;
+        RotateRight = defaults.RotateRight;
+        Cast = defaults.Cast;
+        Pause = defaults.Pause;
+
+        Debug.Log("Binds Reset");
+
+    }
+
+    
+    #if (UNITY_EDITOR)
+    [CustomEditor(typeof(InputScript))]
+    public class InputScriptEditor : Editor {
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+            
+            if (GUILayout.Button("Set to default")) {
+                InputScript thisScript = (InputScript) target;
+                thisScript.resetToDefault();
+            }
+        }
+    }
+    #endif
 
 }
