3992988083c925d0e82b43be64c20eccdecabf1d
three-halves <threehalves1@gmail.com>
2023-04-25 22:37:14 -0500

    start work on solo menu tabs

---
+++ Assets/Scripts/SoloMode/SoloMenuTab.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SoloMode {
+    [CreateAssetMenu(fileName = "MenuTab", menuName = "ManaCycle/MenuTabs")]
+    public class SoloMenuTab : ScriptableObject {
+        [SerializeField] public string tabName;
+        [SerializeField] public Level[] levelsList;
+    }
+}
