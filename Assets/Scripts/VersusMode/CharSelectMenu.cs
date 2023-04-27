6c02de9e0f31bedfb9374838c9c137f7dff92a71
caesarj <jackachulian@gmail.com>
2023-04-27 08:12:57 -0500

    started working on new versus menu

---
+++ Assets/Scripts/VersusMode/CharSelectMenu.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace VersusMode {
+    public class CharacterSelector : MonoBehaviour {
+        [SerializeField] private CharSelectCursor p1cursor;
+        [SerializeField] private CharSelectCursor p2cursor;
+    }
+}
