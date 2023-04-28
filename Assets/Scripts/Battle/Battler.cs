91ec3ede9ca3ee866ab8cf90fc5ac33bc9a5a0ca
caesarj <jackachulian@gmail.com>
2023-04-28 13:25:51 -0500

    battler ability frameworks

--- Assets/Scripts/Battle/Battler.cs
+++ Assets/Scripts/Battle/Battler.cs
@@ -6,11 +6,22 @@ namespace Battle {
     [CreateAssetMenu(fileName = "Battler", menuName = "ManaCycle/Battler")]
     public class Battler : ScriptableObject {
         [SerializeField] public string displayName;
-
         [SerializeField] public Sprite sprite;
 
+        /// <summary>Offset of the portrait in the battle view</summary>
         [SerializeField] public Vector2 portraitOffset;
 
+        /// <summary>Description this battler's passive ability</summary>
+        [SerializeField] public String passiveAbilityDesc;
+        /// <summary>Name of this character's active ability</summary>
+        [SerializeField] public String activeAbilityName;
+        /// <summary>Describes this battler's active ability</summary>
+        [SerializeField] public String activeAbilityDesc;
+        /// <summary>Describes this character's ability in singleplayer mode, if it is different. Blank for same desc</summary>
+        [SerializeField] public String soloAbilityDesc;
+        /// <summary>Amount of mana required to use active ability</summary>
+        [SerializeField] public int activeAbilityMana;
+        /// <summary>The piece RNG used for this battler</summary>
         [SerializeField] public PieceRng pieceRng;
 
         [SerializeField] public AudioClip voiceSFX;
