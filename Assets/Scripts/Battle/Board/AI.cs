9130b4b72d6fecde9be00848fe98122680ceb46e
caesarj <jackachulian@gmail.com>
2023-05-10 11:45:10 -0500

    fixed ai bugs, ai is pretty smart now

--- Assets/Scripts/Battle/Board/AI.cs
+++ Assets/Scripts/Battle/Board/AI.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using Unity.Jobs;
 using UnityEngine.Jobs;
@@ -21,7 +22,7 @@ namespace Battle.Board {
         // public NativeArray<VirtualTile> virtualTiles;
 
         // inner
-        int startOffset;
+        int rngConstant;
 
         ManaColor center, top, right;
         int tileIndex;
@@ -36,7 +37,7 @@ namespace Battle.Board {
 
         public int manaGain;
 
-        public BestPlacementJob(GameBoard board, NativeArray<int> bestPlacement, int minCol=0, int maxCol = 7) {
+        public BestPlacementJob(GameBoard board, NativeArray<int> bestPlacement, int preferredColumn = -1) {
             this.bestPlacement = bestPlacement;
 
             // copy down the state of the board's tiles' colors
@@ -59,101 +60,120 @@ namespace Battle.Board {
             // randomly offset the starting point
             // purpose is to mox in some of the other best placements from the other side of the board
             // that might also earn the max 3 mana gain instead of all left/right
-            startOffset = Random.Range(0, 32);
+            rngConstant = UnityEngine.Random.Range(0, 32);
         }
 
 
         // Tries one of the piece placements.
         // Broken up intoa  seperate job for each to allow CPU workers to do other stuff during the calculation
         public void Execute() {
-            for (int i=startOffset; i<startOffset+32; i++) {
-                int col = (i/4)%8;
-                int rot = i%4;
+            var cols = HighestAndLowestColumns();
+            var difference = cols.Item2 - cols.Item1;
 
-                // A list of mana colors with their positions as opposed to a full array,
-                // since this will only be holding 3 tiles
-                var virtualTiles = new NativeArray<VirtualTile>(3, Allocator.Temp);
-
-                invalidPlacement = false;
-                manaGain = 0;
-                tileIndex = 0;
-
-                // Tiles should be placed in reverse order of row so gravity happens in the right order.
-                switch (rot)
-                {
-                    case 0: // up
-                        // tiles[0] = new VirtualTile(row, col, center);
-                        SpawnTile(col, center, virtualTiles); // row
-                        // tiles[2] = new VirtualTile(row, col+1, right);
-                        SpawnTile(col+1, right, virtualTiles); // row
-                        // tiles[1] = new VirtualTile(row-1, col, top);
-                        SpawnTile(col, top, virtualTiles); // row-1
-                        break;
-                    case 1: // left
-                        // tiles[0] = new VirtualTile(row, col, center);
-                        SpawnTile(col, center, virtualTiles); // row
-                        // tiles[1] = new VirtualTile(row, col-1, top);
-                        SpawnTile(col-1, top, virtualTiles); // row
-                        // tiles[2] = new VirtualTile(row-1, col, right);
-                        SpawnTile(col, right, virtualTiles); // row-1
-                        break;
-                    case 2: // down
-                        // tiles[1] = new VirtualTile(row+1, col, top);
-                        SpawnTile(col, top, virtualTiles); // row+1
-                        // tiles[0] = new VirtualTile(row, col, center);
-                        SpawnTile(col, center, virtualTiles);
-                        // tiles[2] = new VirtualTile(row, col-1, right);
-                        SpawnTile(col-1, right, virtualTiles); // row
-                        break;
-                    case 3: // right
-                        // tiles[2] = new VirtualTile(row+1, col, right);
-                        SpawnTile(col, right, virtualTiles); // row+1
-                        // tiles[0] = new VirtualTile(row, col, center);
-                        SpawnTile(col, center, virtualTiles);
-                        // tiles[1] = new VirtualTile(row, col+1, top);
-                        SpawnTile(col+1, top, virtualTiles); // row
-                        break;
+            // If the highest and lowest columns are more than 5 rows apart, target the lowest column
+            if (difference > 5) {
+                int col = cols.Item2;
+                
+                int rng = rngConstant%4;
+                for (int i = rng; i < rng+4; i++) {
+                    int rot = i%4;
+                    SimulatePlacement(col, rot);
                 }
+            } 
+            
+            // if top and bottom cols are 5 or less apart, safe enough to check all columns for best placement
+            else {
+                for (int i=rngConstant; i<rngConstant+32; i++) {
+                    int col = (i/4)%8;
+                    int rot = i%4;
+                    SimulatePlacement(col, rot);
+                }
+            }
+        }
 
-                // if piece was found to be in an invalid position, do not set stats
-                if (invalidPlacement) {
-                    // Debug.Log(col+", "+rot+": Invalid");
-                    virtualTiles.Dispose();
+        void SimulatePlacement(int col, int rot) {
+            // A list of mana colors with their positions as opposed to a full array,
+            // since this will only be holding 3 tiles
+            var virtualTiles = new NativeArray<VirtualTile>(3, Allocator.Temp);
+
+            invalidPlacement = false;
+            manaGain = 0;
+            tileIndex = 0;
+
+            // Tiles should be placed in reverse order of row so gravity happens in the right order.
+            switch (rot)
+            {
+                case 0: // up
+                    // tiles[0] = new VirtualTile(row, col, center);
+                    SpawnTile(col, center, virtualTiles); // row
+                    // tiles[2] = new VirtualTile(row, col+1, right);
+                    SpawnTile(col+1, right, virtualTiles); // row
+                    // tiles[1] = new VirtualTile(row-1, col, top);
+                    SpawnTile(col, top, virtualTiles); // row-1
                     break;
-                }
-
-                int highestRow = 18;
-                for (int v=0; v<3; v++) {
-                    highestRow = Mathf.Min(virtualTiles[v].row, highestRow);
-                }
-
-                // // If any of the tiles are above row 6, ignore this placement, too dangerous
-                // if (highestRow < 6) continue;
-
-                if (highestRow >= bestPlacement[3]) {
-                    // if highestRow is the same, manaGain is the tiebreaker.
-                    // if manaGain is lower, do not consider this placement
-                    if (highestRow == bestPlacement[3] && manaGain < bestPlacement[2]) {
-                        continue;
-                    }
-
-                    // If above row 8 and earns 0 mana, do not place here
-                    // Should keep it from killing itself more often
-                    if (manaGain > 1 || highestRow >= 8) {
-                        bestPlacement[0] = col;
-                        bestPlacement[1] = rot;
-                        bestPlacement[2] = manaGain;
-                        bestPlacement[3] = highestRow;
-                    }
-
-                    // possible optimization: stop when 3 is reached. not implementing for 2 reasons rn
-                    // - job system goes through all indexes, idk how to avoid that yet
-                    // - may make the algorithm less fully random
-                }
-                virtualTiles.Dispose();
+                case 1: // left
+                    // tiles[0] = new VirtualTile(row, col, center);
+                    SpawnTile(col, center, virtualTiles); // row
+                    // tiles[1] = new VirtualTile(row, col-1, top);
+                    SpawnTile(col-1, top, virtualTiles); // row
+                    // tiles[2] = new VirtualTile(row-1, col, right);
+                    SpawnTile(col, right, virtualTiles); // row-1
+                    break;
+                case 2: // down
+                    // tiles[1] = new VirtualTile(row+1, col, top);
+                    SpawnTile(col, top, virtualTiles); // row+1
+                    // tiles[0] = new VirtualTile(row, col, center);
+                    SpawnTile(col, center, virtualTiles); // row
+                    // tiles[2] = new VirtualTile(row, col-1, right);
+                    SpawnTile(col-1, right, virtualTiles); // row
+                    break;
+                case 3: // right
+                    // tiles[2] = new VirtualTile(row+1, col, right);
+                    SpawnTile(col, right, virtualTiles); // row+1
+                    // tiles[0] = new VirtualTile(row, col, center);
+                    SpawnTile(col, center, virtualTiles);
+                    // tiles[1] = new VirtualTile(row, col+1, top);
+                    SpawnTile(col+1, top, virtualTiles); // row
+                    break;
             }
 
-            // Debug.Log(bestPlacement[0]+", "+bestPlacement[1]+": "+manaGain+", "+bestPlacement[3]);
+            // if piece was found to be in an invalid position, do not set stats
+            if (invalidPlacement) {
+                // Debug.Log(col+", "+rot+": Invalid");
+                virtualTiles.Dispose();
+                return;
+            }
+
+            int highestRow = 18;
+            for (int v=0; v<3; v++) {
+                highestRow = Mathf.Min(virtualTiles[v].row, highestRow);
+            }
+
+            // // If any of the tiles are above row 6, ignore this placement, too dangerous
+            // if (highestRow < 6) continue;
+
+            if (highestRow >= bestPlacement[3]) {
+                // if highestRow is the same, manaGain is the tiebreaker.
+                // if manaGain is lower, do not consider this placement
+                if (highestRow == bestPlacement[3] && manaGain < bestPlacement[2]) {
+                    return;
+                }
+
+                // If above row 8 and earns 0 mana, do not place here
+                // Should keep it from killing itself more often
+                if (manaGain > 1 || highestRow >= 8) {
+                    bestPlacement[0] = col;
+                    bestPlacement[1] = rot;
+                    bestPlacement[2] = manaGain;
+                    bestPlacement[3] = highestRow;
+                }
+
+                // possible optimization: stop when 3 is reached. not implementing for 2 reasons rn
+                // - job system goes through all indexes, idk how to avoid that yet
+                // - may make the algorithm less fully random
+            }
+
+            virtualTiles.Dispose();
         }
 
         // Place a tile into the passed virtual tile list.
@@ -181,7 +201,7 @@ namespace Battle.Board {
                     return;
                 }
 
-                // Check if there's a tile on the board or in virtual tiles
+                // Check if there's a tile below on the board or in virtual tiles
                 // Loop through all virtual tiles to check if it's here - shouldn't take long
                 // as it's only 3 elements long
                 bool virtualTileOccupied = false;
@@ -190,7 +210,7 @@ namespace Battle.Board {
                         // this virtual tile's row will be 0 on init, and at least 1 once placed & fallen
                         tiles[i].row > 2 
                         // check if this virtual tile is in row below
-                        && tiles[i].row == r+1 
+                        && tiles[i].row == r+1
                         // check if this virtual tile is in correct column
                         && tiles[i].col == col) 
                     {
@@ -201,7 +221,7 @@ namespace Battle.Board {
 
                 // if a virtual tile was occupied or tile exists on the actual gameboard, 
                 // place new tile above it
-                if (virtualTileOccupied || boardTiles[r*GameBoard.width + col] != ManaColor.None) {
+                if (virtualTileOccupied || boardTiles[(r+1)*GameBoard.width + col] != ManaColor.None) {
                     PlaceTile(tiles, r, col, color);
                     return;
                 }
@@ -225,7 +245,7 @@ namespace Battle.Board {
                 manaGain++;
             } else {
                 tileIndex++;
-            }  
+            }
         }
 
         // Check the board's tiles and virutal tiles if the color is matched.
@@ -235,7 +255,7 @@ namespace Battle.Board {
             if (boardTiles[row * GameBoard.width + col] == color) return true;
 
             for (int i=0; i<tileIndex; i++) {
-                if (virtualTiles[i].row == row && virtualTiles[i].col == col) return true;
+                if (virtualTiles[i].row == row && virtualTiles[i].col == col && virtualTiles[i].color == color) return true;
             }
 
             return false;
@@ -255,6 +275,41 @@ namespace Battle.Board {
             public override string ToString() {
                 return "("+row+", "+col+")";
             }
+        }
+
+        // Return the column numbers for the hgihest and lowest columns.
+        // Start with a random offset, to include more randomness
+        (int, int) HighestAndLowestColumns() {
+            // first item is highest column (lowest row value)
+            // second item is lowest column (highest row value)
+            var heights = (18, 0);
+            var columns = (-1, -1);
+
+            int offset = rngConstant%8;
+            for (int c = offset; c < GameBoard.width+offset; c++) {
+                var height = ColumnHeight(c%8);
+
+                if (height < heights.Item1) {
+                    heights.Item1 = height;
+                    columns.Item1 = c;
+                }
+                if (height > heights.Item2) {
+                    heights.Item2 = height;
+                    columns.Item2 = c;
+                }
+            }
+
+            return columns;
+        }
+
+        // Return the row height of the tallest column.
+        int ColumnHeight(int col) {
+            for (int row = GameBoard.height - 1; row >= 0; row--) {
+                if (boardTiles[row*GameBoard.width + col] == ManaColor.None) {
+                    return row;
+                }
+            }
+            return 0;
         }
     }
 }
