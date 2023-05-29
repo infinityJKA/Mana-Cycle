9ac4b3b959277941e1f8bc9182dfbaef17b17e42
Jackachulian <jackachulian@gmail.com>
2023-05-29 12:02:49 -0500

    solomenu is preloaded Going to move this functionality to its own class next

---
+++ Assets/Scripts/ScenePreloader.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEngine.UI;
+
+/// <summary>
+/// Preloads scenes into a dictionary of async handlers. Scnees can be pre-loaded, unloaded, and accessed from here.
+/// </summary>
+public class ScenePreloader
+{
+    // Dictionary<string, AsyncOperation> preloaded;
+
+    // public AsyncOperation Preload(string sceneName) {
+    //     // if already preloading, don't preload again
+    //     if (preloaded.ContainsKey(sceneName)) return;
+
+        
+    // }
+}
