271dd51d981ce15d8acabdef710c484a407f5ad5
three-halves <threehalves1@gmail.com>
2023-06-16 00:34:42 -0500

    work on shop layout

---
+++ Assets/Scripts/SoloMode/ArcadeEndless/ItemDisplay.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ItemDisplay : MonoBehaviour
+{
+    // the item this display represents
+    [SerializeField] private Item item;
+
+    [SerializeField] private Image displayImage;
+    [SerializeField] private TextMeshProUGUI nameText;
+    [SerializeField] private TextMeshProUGUI costText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        displayImage.sprite = item.icon;
+        nameText.text = item.itemName;
+        costText.text = "" + item.cost;
+    }
+
+}
