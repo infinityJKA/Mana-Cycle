06a6c6e786ccdfbea859e26e0f50e5b1c6e307ac
Jackachulian <jackachulian@gmail.com>
2023-12-20 16:55:28 -0600

    battle input system, solo keyboard controls only

--- Assets/Scripts/Battle/Controller.cs
+++ Assets/Scripts/Battle/Controller.cs
@@ -24,16 +24,26 @@ namespace Battle {
         // This is NOT the same as single button presses such as the arrow keys.
         private Vector2 movementInput;
 
+        // Whether or not to respond to Input Script update-based inputs instead of the new action input system stuff
+        [SerializeField] private bool battleUseInputScripts = false;
+        [SerializeField] private bool menuUseInputScripts = true;
+
         // Update is called once per frame
         void Update()
         {
+            // if not using tinput scripts, this script's update logic is not needed. (OnMove etc will still work when disabled, innvoked by playerinput script on same object.)
+            if (!battleUseInputScripts && !menuUseInputScripts) {
+                enabled = false;
+                return;
+            }
+
             // stop movement while uninitialized, paused, post game or dialogue
             if (!board.isInitialized() || board.convoPaused) return;
             // note that this doesn't check if boaard is player controlled
             // this is so the player can control the menu in AI vs AI mode
 
             // these will be set to true if any of the input scripts trigger it
-            if (board.IsPlayerControlled()) {
+            if (battleUseInputScripts && board.IsPlayerControlled()) {
                 if (!board.Mobile) board.quickFall = false;
             }
 
@@ -43,7 +53,7 @@ namespace Battle {
  
                 if (inputScript != null)
                 {
-                    if (Input.GetKeyDown(inputScript.Pause) && !board.postGame && !Storage.convoEndedThisInput)
+                    if (menuUseInputScripts && Input.GetKeyDown(inputScript.Pause) && !board.postGame && !Storage.convoEndedThisInput)
                     {
                         board.pauseMenu.TogglePause();
                         board.PlaySFX("pause", pan: 0);
@@ -51,7 +61,7 @@ namespace Battle {
                     Storage.convoEndedThisInput = false;
 
                     // control the pause menu if paused
-                    if (board.pauseMenu.paused && !board.postGame)
+                    if (menuUseInputScripts && board.pauseMenu.paused && !board.postGame)
                     {
                         if (Input.GetKeyDown(inputScript.Up)) {
                             board.pauseMenu.MoveCursor(Vector3.up);
@@ -67,7 +77,7 @@ namespace Battle {
                     }
 
                     // same with post game menu, if timer is not running
-                    else if (board.postGame && !board.winMenu.timerRunning)
+                    else if (menuUseInputScripts && board.postGame && !board.winMenu.timerRunning)
                     {
                         if (board.winMenu == null) return;
                         if (Input.GetKeyDown(inputScript.Up)) {
@@ -85,7 +95,7 @@ namespace Battle {
                     else if (board.recoveryMode || !board.IsPlayerControlled()) continue;
                     
                     // If not pausemenu paused, do piece movements if not dialogue paused and not in postgame
-                    else if (!board.isPaused() && !board.isPostGame()) {
+                    else if (battleUseInputScripts && !board.isPaused() && !board.isPostGame()) {
                         // code previously in controller.cs
                         if (Input.GetKeyDown(inputScript.RotateCW)){
                             board.RotateLeft();
@@ -128,6 +138,38 @@ namespace Battle {
         // Functions to handle new Action Input System invocations
         public void OnMove(InputAction.CallbackContext ctx) {
             movementInput = ctx.ReadValue<Vector2>();
+        }
+
+        public void OnQuickfall(InputAction.CallbackContext ctx) {
+            if (ctx.started) {
+                board.quickFall = true;
+                board.instaDropThisFrame = true;
+                Debug.Log("quickfalling");
+            }
+            if (ctx.canceled) {
+                board.quickFall = false;
+                Debug.Log("stopped quickfalling");
+            }
+        }
+
+        public void PieceTapLeft(InputAction.CallbackContext ctx) {
+            if (ctx.performed) board.MoveLeft();
+        }
+
+        public void PieceTapRight(InputAction.CallbackContext ctx) {
+            if (ctx.performed) board.MoveRight();
+        }
+
+        public void OnRotateLeft(InputAction.CallbackContext ctx) {
+            if (ctx.performed) board.RotateLeft();
+        }
+
+        public void OnRotateRight(InputAction.CallbackContext ctx) {
+            if (ctx.performed) board.RotateRight();
+        }
+
+        public void OnSpellcast(InputAction.CallbackContext ctx) {
+            if (ctx.performed) board.Spellcast();
         }
     }
 }
