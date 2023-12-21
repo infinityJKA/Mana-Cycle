717e57fe763cbcb039a8c91d4dac939a4cb3db7d
Jackachulian <jackachulian@gmail.com>
2023-12-20 20:01:09 -0600

    PlayerConnectionManager, charselect control works

--- Assets/Scripts/Multiplayer/PlayerConnectionManager.cs
+++ Assets/Scripts/Multiplayer/PlayerConnectionManager.cs
@@ -3,6 +3,7 @@ using UnityEngine.InputSystem;
 
 using Battle;
 using Battle.Board;
+using VersusMode;
 
 namespace Multiplayer {
     public class PlayerConnectionManager : MonoBehaviour {
@@ -10,31 +11,59 @@ namespace Multiplayer {
 
         [SerializeField] private GameBoard[] boards;
 
-        [SerializeField] private bool isBattle;
+        [SerializeField] private CharSelector[] charSelectors;
 
+        [SerializeField] private ConnectMode connectMode;
+
+        public enum ConnectMode {
+            CharSelect,
+            Battle,
+            // If this mode is found, then remove all the players when this object is loaded, destroying both this and current instance
+            // depending on how things go, this may be removed and main menu will have multiplayer support, or at least repsond to multiple controllers differently
+            DestroyMultiplayer, 
+        }
+
+        // if player joined script should reparent to currently invoked connection manager or not
+        // (OnPlayerJoined can only have 1 argument because it's called by a unity event in Player Input Manager)
+        private bool reparent = false;
 
         private void Awake() {
             if (instance == null) {
-                instance = this;
+                if (connectMode == ConnectMode.DestroyMultiplayer) {
+                    Destroy(gameObject);
+                } else {
+                    instance = this;
+                    reparent = true;
+                    DontDestroyOnLoad(gameObject);
+                }
             } else {
-                // If within a battle scene, this means that this is loading in while switching between charselect > battle.
-                // Connect each controller to its respective board.
-                if (isBattle) {
+                reparent = false;
+
+                if (connectMode == ConnectMode.DestroyMultiplayer) {
+                    Destroy(instance.gameObject);
+                } else {
                     foreach (var playerInput in instance.transform.GetComponentsInChildren<PlayerInput>()) {
-                        ConnectControllerToBoard(playerInput);
+                        OnPlayerJoined(playerInput);
                     }
                 }
                 
-                // after all this, destroy this gameobject since the existing one should be the only one.
+                // after all this tomfoolery, destroy this gameobject since the existing one should be the only one.
                 Destroy(gameObject);
             }
         }
         
+        // Initialize based on THIS connection manager's connect mode.
+        // Will not reparent when called from awake while an instance already exists in which all the player inputs are parented
+        // however this will contain info needed by the existing connection manager to set up connections before the new one is destroyed
         public void OnPlayerJoined(PlayerInput playerInput) {
             // for persistence between scenes (and organization), parent to this object
-            playerInput.transform.SetParent(transform);
+            if (reparent) playerInput.transform.SetParent(transform);
 
-            if (boards != null) {
+            if (connectMode == ConnectMode.CharSelect) {
+                ConnectControllerToCharSelector(playerInput);
+            }
+
+            else if (connectMode == ConnectMode.Battle) {
                 ConnectControllerToBoard(playerInput);
             }
         }
@@ -44,6 +73,11 @@ namespace Multiplayer {
             
             var controller = playerInput.GetComponent<Controller>();
             controller.SetBoard(boards[playerInput.playerIndex]);
+        }
+
+        public void ConnectControllerToCharSelector(PlayerInput playerInput) {
+            var controller = playerInput.GetComponent<Controller>();
+            controller.SetCharSelector(charSelectors[playerInput.playerIndex]);
         }
     }
 }
