2545815a8f8c84a37d5aecb5c4d6ff4d3d548346
Justout <jkajustin@gmail.com>
2023-12-24 14:44:27 -0600

    super WIP fishing overworld scene

---
+++ Assets/Scripts/Fishing/FishingSceneCam.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishingSceneCam : MonoBehaviour
+{
+    private Vector3 offset;
+    public Transform target;
+    public float smoothTime;
+    private Vector3 currentVelocity = Vector3.zero;
+
+    private void Awake(){
+        offset = transform.position - target.position;
+    }
+
+    private void Update(){
+        Vector3 targetPos = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref currentVelocity, smoothTime);
+    }
+
+}
