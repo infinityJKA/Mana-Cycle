4aa6c8a20322ae7a99d8c55d8270992fb127ea04
Justout <jkajustin@gmail.com>
2023-12-24 17:59:35 -0600

    Infinity moves and has proper collision/gravity

--- Assets/Scripts/Fishing/FishingPlayer.cs
+++ Assets/Scripts/Fishing/FishingPlayer.cs
@@ -8,14 +8,20 @@ public class FishingPlayer : MonoBehaviour
     public float walkSpeed = 5f;
     public float turningTime = 0.1f;
     public float turnSpeed;
+    private float gravity;
 
     void Update(){
+        // Gravity
+        if ( controller.isGrounded ) gravity = 0;
+        else gravity -= 9.81f* Time.deltaTime;
+
         // Take in inputs (idk if the new input system will replace this)
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 direction = new Vector3(horizontal, gravity, vertical).normalized;
 
         if(direction.magnitude >= 0.1f){
+
             // Calculate and smooth the angle the player will face
             float targetAngle = Mathf.Atan2(direction.x,direction.z)*Mathf.Rad2Deg;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSpeed, turningTime);
