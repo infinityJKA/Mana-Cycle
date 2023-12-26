d9c97eb2c228cd7f95a35fa6feb1e8ea483c26e5
Justout <jkajustin@gmail.com>
2023-12-25 18:03:10 -0600

    WIP Inventory display

--- Assets/Scripts/Fishing/FishingItem.cs
+++ Assets/Scripts/Fishing/FishingItem.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FishingItem : ScriptableObject
 {
     public string itemName = "itemName";
     public int buyValue = 0;
     public int sellValue = 0;
+    public Sprite icon;
 }
