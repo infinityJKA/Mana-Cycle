d54d3a3f3023229315cc6a99d95f5a19a75f3579
Jackachulian <jackachulian@gmail.com>
2023-12-30 15:00:18 -0600

    Control graphic update, wip

--- Assets/Scripts/Input/InputPrompt.cs
+++ Assets/Scripts/Input/InputPrompt.cs
@@ -5,18 +5,36 @@ using UnityEngine.InputSystem.Users;
 using UnityEngine.UI;
 
 public class InputPrompt : MonoBehaviour {
-    [SerializeField] private InputUser user;
+    [SerializeField] private Image image;
 
-    [SerializeField] private Image keyboardImage, gamepadImage;
+    [SerializeField] private Sprite keyboardSprite, keyboardPressedSprite, xboxSprite, xboxPressedSprite, playstationSprite, playstationPressedSprite;
 
-    private bool listening = false;
+    private Sprite unpressedSprite, pressedSprite;
+
+    [SerializeField] private InputActionReference inputActionReference;
+
+    public void Press(InputAction.CallbackContext ctx) {
+        image.sprite = pressedSprite;
+    }
+
+    public void Unpress(InputAction.CallbackContext ctx) {
+        image.sprite = unpressedSprite;
+    }
 
     private void OnEnable() {
         InputSystem.onDeviceChange += OnDeviceChange;
+        if (inputActionReference != null) {
+            inputActionReference.action.started += Press;
+            inputActionReference.action.canceled += Unpress;
+        }
     }
 
     private void OnDisable() {
         InputSystem.onDeviceChange -= OnDeviceChange;
+        if (inputActionReference != null) {
+            inputActionReference.action.started -= Press;
+            inputActionReference.action.canceled -= Unpress;
+        }
     }
 
     public void OnDeviceChange(InputDevice device, InputDeviceChange change) {
@@ -29,5 +47,24 @@ public class InputPrompt : MonoBehaviour {
     public void OnControlsChanged(PlayerInput playerInput) {
         Debug.Log($"Input device changed for player {playerInput.playerIndex}: {playerInput.devices[0].name}");
         InputDevice newDevice = playerInput.devices[0];
+
+        if (playerInput.currentControlScheme == "Keyboard&Mouse") {
+            unpressedSprite = keyboardSprite;
+            pressedSprite = keyboardPressedSprite;
+        } else if (playerInput.currentControlScheme == "Gamepad") {
+            if (newDevice.description.product.ToLower().Contains("playstation")) {
+                unpressedSprite = playstationSprite;
+                pressedSprite = playstationPressedSprite;
+            } else if (newDevice.description.product.ToLower().Contains("")) {
+                unpressedSprite = xboxSprite;
+                pressedSprite = xboxPressedSprite;
+            }
+        }
+
+        if (inputActionReference != null) {
+            image.sprite = inputActionReference.action.IsPressed() ? pressedSprite : unpressedSprite;
+        } else {
+            image.sprite = unpressedSprite;
+        }
     }
 }
--- Assets/Scripts/Input/InputPromptGroup.cs
+++ Assets/Scripts/Input/InputPromptGroup.cs
@@ -6,6 +6,7 @@ public class InputPromptGroup : MonoBehaviour {
     [SerializeField] public List<InputPrompt> prompts;
 
     public void OnControlsChanged(PlayerInput playerInput) {
+        Debug.Log("Controls changed! "+playerInput.devices[0].name);
         foreach (InputPrompt inputPrompt in prompts) {
             inputPrompt.OnControlsChanged(playerInput);
         }
