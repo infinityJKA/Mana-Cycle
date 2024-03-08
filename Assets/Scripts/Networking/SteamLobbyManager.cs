96c88bab34c278e3d6a1cf15dbdb9022bf529d0b
Jackachulian <jackachulian@gmail.com>
2024-03-08 15:41:11 -0600

    steam manager fixes

--- Assets/Scripts/Networking/SteamLobbyManager.cs
+++ Assets/Scripts/Networking/SteamLobbyManager.cs
@@ -7,22 +7,39 @@ using Steamworks;
 #endif
 
 namespace Networking {
-    public class SteamLobbyManager {
+    public class SteamLobbyManager : MonoBehaviour {
+
+        public static SteamLobbyManager instance {get; private set;}
 
         #if !DISABLESTEAMWORKS
-        protected static Callback<LobbyCreated_t> lobbyCreated;
-        protected static Callback<GameLobbyJoinRequested_t> gameLobbyJoinRequested;
-        protected static Callback<LobbyEnter_t> lobbyEntered;
+        protected Callback<LobbyCreated_t> lobbyCreated;
+        protected Callback<GameLobbyJoinRequested_t> gameLobbyJoinRequested;
+        protected Callback<LobbyEnter_t> lobbyEntered;
         private const string HostAddressKey = "HostAddress";
-        public static bool steamInitialized {get; private set;} = false;
+        public bool steamInitialized {get; private set;} = false;
         #endif
 
-        #if !DISABLESTEAMWORKS
-        public static void InitializeSteam() {
-            if (NetworkManagerManager.networkManagerType != NetworkManagerManager.NetworkManagerType.Steam) {
-                Debug.LogWarning("Trying to use steam, but it is disabled");
+        private void Start() {
+            if (instance != null) {
+                Destroy(gameObject);
+                return;
             }
+            instance = this;
+            DontDestroyOnLoad(this);
+            InitializeSteam();
+        }
 
+        public void CreateLobby() {
+            #if !DISABLESTEAMWORKS
+                Debug.Log("Starting steam lobby");
+                SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, NetworkManager.singleton.maxConnections);
+            #else
+                Debug.LogError("Trying to start a Steam lobby, but Steamworks is disabled!");
+            #endif
+        }
+
+        #if !DISABLESTEAMWORKS
+        public void InitializeSteam() {
             if (steamInitialized) {
                 Debug.LogWarning("SteamManager already initialized");
                 return;
@@ -39,21 +56,10 @@ namespace Networking {
 
             steamInitialized = true;
         }
-        #endif
 
-        public static void CreateLobby() {
-            #if !DISABLESTEAMWORKS
-                Debug.Log("Starting steam lobby");
-                SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, NetworkManager.singleton.maxConnections);
-            #else
-                Debug.LogError("Trying to start a Steam lobby, but Steamworks is disabled!");
-            #endif
-        }
-
-        #if !DISABLESTEAMWORKS
-        private static void OnLobbyCreated(LobbyCreated_t callback) {
+        private void OnLobbyCreated(LobbyCreated_t callback) {
             if (callback.m_eResult != EResult.k_EResultOK) {
-                Debug.LogError("Error creating steam lobby "+callback.m_eResult);
+                Debug.LogError("Error creating steam lobby: "+callback.m_eResult);
                 OnlineMenu.singleton.ShowOnlineMenu();
                 return;
             }
@@ -61,9 +67,10 @@ namespace Networking {
             NetworkManager.singleton.StartHost();
 
             SteamMatchmaking.SetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), HostAddressKey, SteamUser.GetSteamID().ToString());
+            SteamMatchmaking.SetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), "name", SteamFriends.GetPersonaName().ToString());
         }
 
-        private static void OnGameLobbyJoinRequested(GameLobbyJoinRequested_t callback) {
+        private void OnGameLobbyJoinRequested(GameLobbyJoinRequested_t callback) {
             Debug.Log("Steam lobby join requested!");
             // if joining from steam, make sure the charselect scene is loaded first before joining lobby
             if (SceneManager.GetActiveScene().name != "CharSelect") {
@@ -74,10 +81,12 @@ namespace Networking {
             SteamMatchmaking.JoinLobby(callback.m_steamIDLobby);
         }
 
-        private static void OnLobbyEntered(LobbyEnter_t callback) {
+        private void OnLobbyEntered(LobbyEnter_t callback) {
             if (NetworkServer.active) return; // do not run on host
 
             string hostAddress = SteamMatchmaking.GetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), HostAddressKey);
+
+            if (NetworkServer.active) return;
 
             NetworkManager.singleton.networkAddress = hostAddress;
             NetworkManager.singleton.StartClient();
