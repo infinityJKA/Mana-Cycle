eac33aa9f829e8cbc53d37b79c9aa30f609bed79
mjjm4d <threehalves1@gmail.com>
2024-03-18 16:20:18 -0500

    cycle visual fix

--- Assets/Scripts/Battle/Board/TileVisual.cs
+++ Assets/Scripts/Battle/Board/TileVisual.cs
@@ -12,11 +12,14 @@ namespace Battle.Board {
     public class TileVisual : MonoBehaviour
     {
         [SerializeField] private Image iconImage;
+        [SerializeField] private Image bgImage;
         public void SetVisual(Cosmetics.ManaIcon v, Cosmetics.ManaPalette p)
         {
             Debug.Log("Setting up visual");
             iconImage.sprite = v.iconSprite;
+            bgImage.sprite = v.bgSprite;
             iconImage.color = p.darkColor;
+            bgImage.color = p.mainColor;
             iconImage.rectTransform.anchoredPosition = new Vector2(v.xOffset, v.yOffset);
             iconImage.transform.eulerAngles = new Vector3(0f, 0f, v.rotation);
             iconImage.transform.localScale = new Vector3(v.xScale, v.yScale, 1f);
