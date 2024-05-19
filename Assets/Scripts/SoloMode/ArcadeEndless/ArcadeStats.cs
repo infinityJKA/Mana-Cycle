c88705a6dffa99b58a1461e9c3fd854449a4e484
mjjm4d <threehalves1@gmail.com>
2024-05-19 05:34:06 -0500

    add stats viewer in gauntlet

--- Assets/Scripts/SoloMode/ArcadeEndless/ArcadeStats.cs
+++ Assets/Scripts/SoloMode/ArcadeEndless/ArcadeStats.cs
@@ -68,4 +68,29 @@ public class ArcadeStats
         MoneyMult, // multiplier for money gained in AE. starts at 1x
     }
 
+    public static string StatToString(Stat s)
+    {
+        switch(s)
+        {
+            case Stat.DamageMult: return "Damage Multiplier";
+            case Stat.StartingSpecial: return "Starting Special";
+            case Stat.SpecialGainMult: return "Special Gain Multiplier";
+            case Stat.StartingCycleModifier: return "Starting Cycle Modifier";
+            case Stat.CycleMultIncrease: return "Full Cycle Bonus";
+            case Stat.QuickDropSpeed: return "Quick Drop Speed";
+            case Stat.MoneyMult: return "Money Multiplier";
+            default: return "Unamed Stat";
+        }
+    }
+
+    public static string StatToUnit(Stat s)
+    {
+        switch(s)
+        {
+            case Stat.StartingSpecial: return "%";
+            case Stat.QuickDropSpeed: return "";
+            default: return "x";
+        }
+    }
+
 }
---
+++ Assets/Scripts/SoloMode/ArcadeEndless/StatsWindow.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StatsWindow : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI statText;
+    [SerializeField] private TextMeshProUGUI valueText;
+
+    [SerializeField] private int leftPad = 30;
+
+    [SerializeField] private GameObject openSFX, closeSFX;
+
+    public void Refresh()
+    {
+        statText.text = "";
+        valueText.text = "";
+        foreach (KeyValuePair<ArcadeStats.Stat, float> stat in ArcadeStats.playerStats)
+        {
+            statText.text += ArcadeStats.StatToString(stat.Key) + "\n";
+            for (int i = 0; i < leftPad; i++) valueText.text += " ";
+            valueText.text += (stat.Value + (stat.Key == ArcadeStats.Stat.StartingCycleModifier ? 1 : 0) * (stat.Key == ArcadeStats.Stat.StartingSpecial ? 100 : 1)) + ArcadeStats.StatToUnit(stat.Key) + "\n";
+        }
+    }
+
+    void OnEnable()
+    {
+        Instantiate(openSFX);
+    }
+
+    void OnDisable()
+    {
+        Instantiate(closeSFX);
+    }
+}
