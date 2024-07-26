e6119db6862dd9db802ebd408e30730a3ce9b0ef
mjjm4d <threehalves1@gmail.com>
2024-07-26 13:45:18 -0500

    simple bg option

--- Assets/Scripts/MainMenu/PlayerPrefSetter.cs
+++ Assets/Scripts/MainMenu/PlayerPrefSetter.cs
@@ -5,23 +5,36 @@ using UnityEngine;
 using UnityEngine.UI;
 
 // used on sliders / options to set player prefs through unity event trigger system
-namespace MainMenu{
+namespace MainMenu {
     public class PlayerPrefSetter : MonoBehaviour
     {
         [SerializeField] private string key = "defaultKey";
 
-        // optional slider to sync to settings
+        // optional selectables to sync to settings
         [SerializeField] private Slider slider;
+        [SerializeField] private Toggle toggle;
 
         public void SetPrefFloat(float value)
         {
             PlayerPrefs.SetFloat(key, value);
         }
 
+        public void SetPrefInt(int value)
+        {
+            PlayerPrefs.SetInt(key, value);
+        }
+
+        public void SetPrefBool(bool value)
+        {
+            PlayerPrefs.SetInt(key, value ? 1 : 0);
+        }
+
         public void Sync()
         {
             if (slider != null) slider.value = PlayerPrefs.GetFloat(key);
+            if (toggle != null) toggle.isOn = PlayerPrefs.GetInt(key) == 1;
         }
+
     }
 
 }
